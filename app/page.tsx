4752845c3f3a9653396da0387d6f3b5733d2232a
abuhasanmahmud <mahmud.me420@gmail.com>
2023-08-29 21:18:02 +0600

    Feat(v-1) hero section

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+          {/* <p>Explore out cars you might like</p> */}
         </div>
         <div className="home__filters">
           <SearchBar />
